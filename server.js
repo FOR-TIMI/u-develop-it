6dcdfc000cd04ff3a296d3946877d80e2036d2c6
FOR-TIMI <timiplayskeys@gmail.com>
2022-09-12 23:46:51 -0400

    added route to get all candidates

--- server.js
+++ server.js
@@ -4,12 +4,57 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
+
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+//SQL section
+const mysql = require('mysql2');
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Eriferere1010!',
+      database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+app.get('/api/candidates', (req,res) => {
+    const sql = `SELECT * FROM candidates`;
+    db.query(sql, (err, rows) => {
+        if(err) return res.status(500).json({"Error message": err.message});
+        return res.json({"message" : "success", "data": rows});
+        
+    })
+})
+
+// db.query(`SELECT * FROM candidates`, (err,rows) => {
+//     if (err) console.error(err);
+//     console.log(row);
+// })
+
+
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
+// db.query(`SELECT * FROM candidates`, (err,row) => {
+//     if(err) console.error(err.message);
+//     console.log(row)
+// })
 
   // Default response for any other request (Not Found)
 app.use((req, res) => {
